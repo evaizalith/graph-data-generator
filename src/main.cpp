4743a3f54dace7102cf69dce3f78888dbb5d95e4
FoundIzalith <evapowlison@protonmail.com>
2025-05-18 16:29:30 -0400

    Improve error logging

--- src/main.cpp
+++ src/main.cpp
@@ -249,13 +249,17 @@ int main(int argc, char** argv) {
 
     int flags; 
     glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
-    /*if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
     {
         glEnable(GL_DEBUG_OUTPUT);
         glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); 
         glDebugMessageCallback(glDebugOutput, nullptr);
-        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
-    }*/
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_HIGH, 0, nullptr, GL_TRUE);
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_MEDIUM, 0, nullptr, GL_FALSE);
+
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_LOW, 0, nullptr, GL_FALSE);
+
+    }
 
     genGraph();
 
