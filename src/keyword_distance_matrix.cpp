1aeed3245f55f1051e9c21f698c5ab3896ab23af
FoundIzalith <evapowlison@protonmail.com>
2025-05-26 16:10:48 -0400

    Improve runtime of cpu matrix algorithm

--- src/keyword_distance_matrix.cpp
+++ src/keyword_distance_matrix.cpp
@@ -1,5 +1,7 @@
 #include "keyword_distance_matrix.hpp"
 #include <climits>
+#include <omp.h>
+#include <iostream>
 
 KeywordDistanceMatrix::KeywordDistanceMatrix(int n_W, int n_V) {
     W = n_W;
@@ -12,7 +14,7 @@ KeywordDistanceMatrix::KeywordDistanceMatrix(int n_W, int n_V) {
 
 KeywordDistanceMatrix::~KeywordDistanceMatrix() {
     if (matrix) {
-        for (int i; i < W; i++) {
+        for (int i = 0; i < W; i++) {
             delete[] matrix[i];
         }
 
@@ -27,38 +29,42 @@ Pair* KeywordDistanceMatrix::operator()(int w, int v) {
 }
 
 void KeywordDistanceMatrix::calculate_matrix_cpu(SparseGraph<int>* graph) {
-    for (int w = 0; w < W; w++) {
-        int dist[V];
-        int pred[V];
+    std::vector<VerboseEdge<int>> list = graph->get_edge_list();
 
-        for (int v = 0; v < V; v++) {
-            dist[v] = INT_MAX;
-            pred[v] = -1;
+    #pragma omp parallel 
+    {
+        #pragma omp for
+        for (int w = 0; w < W; w++) {
+            int dist[V];
+            int pred[V];
 
-            if (graph->keyword_is_in(w, v)) {
-                dist[v] = 0;
-                pred[v] = v;
-            }
-        }
+            for (int v = 0; v < V; v++) {
+                dist[v] = INT_MAX;
+                pred[v] = -1;
 
-        // This part of the algorithm is the most run-time intensive and potentially has the most
-        // opportunity for optimization
-        for (int counter = 0; counter < V; counter++) {
-            for (int a = 0; a < V; a++) {
-                std::vector<Edge<int>> edges = graph->get_adjacent(a); 
-                for (int e = 0; e < edges.size(); e++) {
-                    Edge<int> edge = edges[e];
-                    if (dist[a] + edge.weight < dist[edge.end]) {
-                        dist[edge.end] = dist[a] + edge.weight;
-                        pred[edge.end] = a;
-                    }
+                if (graph->keyword_is_in(w, v)) {
+                    dist[v] = 0;
+                    pred[v] = v;
                 }
-            }   
-        }
+            }
 
-        for (int v = 0; v < V; v++) {
-            matrix[w][v].dist = dist[v];
-            matrix[w][v].pred = pred[v];
+            // This part of the algorithm is the most run-time intensive and potentially has the most
+            // opportunity for optimization
+            for (int counter = 0; counter < V; counter++) {
+                for (auto& edge: list) {
+                    if (dist[edge.start] + edge.weight < dist[edge.end]) {
+                        dist[edge.end] = dist[edge.start] + edge.weight;
+                        pred[edge.end] = edge.start;
+                    }
+                }   
+            }
+
+            for (int v = 0; v < V; v++) {
+                matrix[w][v].dist = dist[v];
+                matrix[w][v].pred = pred[v];
+            }
+        int tid = omp_get_thread_num();
+        std::cout << "Thread " << tid << " completed keyword " << w << std::endl;
         }
     }
 }
